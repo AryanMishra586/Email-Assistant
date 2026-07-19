f516ec9b055ae7675a92ff702ba4685517563925
Aryan Mishra <aryanmishra12112003@gmail.com>
2026-07-19 17:29:58 +0530

    Debugging extension

--- email-writer-ext/config.js
+++
@@ -1,7 +0,0 @@
-// Development
-// const API_URL = "http://localhost:8080";
-
-// Production
-const API_URL = "https://email-assistant-774b.onrender.com";
-
-export default API_URL;
--- email-writer-ext/content.js
+++ email-writer-ext/content.js
@@ -1,5 +1,8 @@
-import API_URL from "./config.js";
+// Development
+// const API_URL = "http://localhost:8080";
 
+// Production
+const API_URL = "https://email-assistant-774b.onrender.com";
 
 console.log("Email Writer Assistant Extension - Content Script Loaded");
 
